ee0f1e14f43887f35696b2f19a3f4faea2bd1e6e
tasnuvasejuti <tasnuvasejuti@gmail.com>
2025-09-07 22:25:14 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import BookingForm from "./components/BookingForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrap">
+      <Header />
+      <BookingForm />
+    </div>
+  );
+}
+
+export default App;
